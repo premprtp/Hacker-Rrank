200cd8bf467718fa6d60c31588365988b4b3ee69
Prem Pratap <theprempratap@gmail.com>
2021-12-20 14:39:05 +0530

    1 prblm

---
+++ src/WelcometoJava.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class WelcometoJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
